9672fa9c187ae421b7c15502a08fbd9fbc466a2d
pietroziviani <165380018+pietroziviani@users.noreply.github.com>
2024-04-16 10:57:41 +0200

    Finitoo

--- codice.c
+++ codice.c
@@ -18,13 +18,13 @@ void estrai_dati(int ac, char **av, int *vett, int *lung)
 
 void fai_spazio(int posizione, int *vett, int lung)
 {
-	for (int j = lung - 1; j > posizione; ++j) 
+	for (int j = lung; j > posizione; --j)
 		vett[j] = vett[j-1];
 }
 
 void inserisci(int nuovo_dato, int num_dati_ord, int *vett)
 { 
-	if (num_dati_ord = 0)  { // il vettore è vuoto, facile
+	if (num_dati_ord == 0)  { // il vettore è vuoto, facile
 		vett[0] = nuovo_dato;
 		return;
 	}
@@ -36,14 +36,14 @@ void inserisci(int nuovo_dato, int num_dati_ord, int *vett)
 			fai_spazio(i, vett, num_dati_ord);
 			vett[i] = nuovo_dato;
 			return;
-		}
-	}
+		}	
+	vett[num_dati_ordinati] = nuovo_dato;
+	}	
 }
 
-void ordina_dati(const int *dati_non_ordinati, int *dati_ordinati)
+void ordina_dati(const int *dati_non_ordinati, int *dati_ordinati, int lung)
 {
-	int num_dati = sizeof(dati_non_ordinati) / sizeof(dati_non_ordinati[0]);
-	for (int i = 0; i < num_dati; ++i)
+	for (int i = 0; i < lung; ++i)
 		inserisci(dati_non_ordinati[i], i, dati_ordinati);
 }
 
@@ -65,7 +65,7 @@ int main(int argc, char **argv)
 	int num_dati = 0;
 
 	estrai_dati(argc, argv, dati_input, &num_dati);
-	ordina_dati(dati_input, dati_ordinati);
+	ordina_dati(dati_input, dati_ordinati, num_dati);
 	stampa_vettore(dati_ordinati, num_dati);
 	return 0;
 }
